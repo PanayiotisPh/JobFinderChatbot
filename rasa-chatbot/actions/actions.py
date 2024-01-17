ee6e20af7546ef8cfbe86ce9c2e5010a00622fe1
Panayiotis Photiadis <pfotia01@ucy.ac.cy>
2024-01-17 21:52:07 +0200

    chatbot now expects answers based on the question

--- rasa-chatbot/actions/actions.py
+++ rasa-chatbot/actions/actions.py
@@ -3,14 +3,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
-#TODO
-# main problem is that values can change in any point of the conv even if chatbot ask something else
-# getting action name does not work
-# list is not part of entities but is a part of slots so the below code must change to affect slots
-# how to check when to affect slots is the problem
-# possible solution but not desirable for later is the use of public variables
-# difficult option
-# CHANGE YOUR WHOLE APPROACH
+action = "null"
 
 class ActionCollectInformation(Action):
     def name(self) -> Text:
@@ -20,43 +13,187 @@ class ActionCollectInformation(Action):
         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
         try:
-            intent = tracker.latest_action_name
-            print(intent)
-            # Check the intent and collect information for corresponding slots
-            if intent == "collect_location":
+            global action
+           
+            if action == "collect_location":
                 location = next(tracker.get_latest_entity_values("location"), None)
-                # Perform actions with location information (e.g., store in a database)
-                dispatcher.utter_message(f"Understood! Your location is {location}.")
-            elif intent == "collect_salary":
+                if location == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Understood! Your location is {location}.")
+                    action = "null"
+
+            elif action == "collect_salary":
                 salary = next(tracker.get_latest_entity_values("salary"), None)
-                # Perform actions with salary information (e.g., store in a database)
-                dispatcher.utter_message(f"Got it! Your salary is {salary}.")
-            elif intent == "collect_job_type":
+                if salary == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Got it! Your salary is {salary}.")
+                    action = "null"
+
+            elif action == "collect_job_type":
+                action = "null"
                 job_type = next(tracker.get_latest_entity_values("job_type"), None)
-                # Perform actions with job_type information (e.g., store in a database)
-                dispatcher.utter_message(f"Noted! Your job type is {job_type}.")
-            elif intent == "collect_company":
+                if job_type == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Noted! Your job type is {job_type}.")
+                    action = "null"
+
+            elif action == "collect_company":
                 company = next(tracker.get_latest_entity_values("company"), None)
-                # Perform actions with company information (e.g., store in a database)
-                dispatcher.utter_message(f"Got it! Your company is {company}.")
-            elif intent == "collect_year_of_xp":
+                if company == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Got it! Your company is {company}.")
+                    action = "null"
+
+            elif action == "collect_year_of_xp":
                 year_of_xp = next(tracker.get_latest_entity_values("year_of_xp"), None)
-                # Perform actions with year_of_xp information (e.g., store in a database)
-                dispatcher.utter_message(f"Sure! Your years of experience are {year_of_xp}.")
-            elif intent == "collect_education":
+                if year_of_xp == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Sure! Your years of experience are {year_of_xp}.")
+                    action = "null"
+
+            elif action == "collect_education":
                 education = next(tracker.get_latest_entity_values("education"), None)
-                # Perform actions with education information (e.g., store in a database)
-                dispatcher.utter_message(f"Great! Your education level is {education}.")
-            elif intent == "collect_soft_skills":
+                if education == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Great! Your education level is {education}.")
+                    action = "null"
+
+            elif action == "collect_soft_skills":
                 soft_skills = next(tracker.get_latest_entity_values("soft_skills"), None)
-                # Perform actions with soft_skills information (e.g., store in a database)
-                dispatcher.utter_message(f"Nice! Your soft skills are {soft_skills}.")
-            elif intent == "collect_hard_skills":
+                if soft_skills == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Nice! Your soft skills are {soft_skills}.")
+                    action = "null"
+
+            elif action == "collect_hard_skills":
                 hard_skills = next(tracker.get_latest_entity_values("hard_skills"), None)
-                # Perform actions with hard_skills information (e.g., store in a database)
-                dispatcher.utter_message(f"Got it! Your hard skills are {hard_skills}.")
+                if hard_skills == None:
+                    dispatcher.utter_message(f"Sorry, I didn't get that. Can you rephrase it?")
+                else:
+                    dispatcher.utter_message(f"Got it! Your hard skills are {hard_skills}.")
+                    action = "null"
 
             return []
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+        
+
+class ActionSetLocation(Action):
+    def name(self) -> Text:
+        return "action_set_location"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_location"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+        
+class ActionSetSalary(Action):
+    def name(self) -> Text:
+        return "action_set_salary"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_salary"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+        
+class ActionSetJobType(Action):
+    def name(self) -> Text:
+        return "action_set_job_type"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_job_type"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+
+class ActionSetCompany(Action):
+    def name(self) -> Text:
+        return "action_set_company"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_company"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+        
+class ActionSetYearOfXp(Action):
+    def name(self) -> Text:
+        return "action_set_year_of_xp"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_year_of_xp"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+        
+class ActionSetEducation(Action):
+    def name(self) -> Text:
+        return "action_set_education"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_education"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+
+class ActionSetSoftSkills(Action):
+    def name(self) -> Text:
+        return "action_set_soft_skills"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_soft_skills"
+        except Exception as e:
+            print(f"Exception: {str(e)}")
+            return []
+        
+class ActionSetHardSkills(Action):
+    def name(self) -> Text:
+        return "action_set_hard_skills"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            global action
+            action = "collect_hard_skills"
         except Exception as e:
             print(f"Exception: {str(e)}")
             return []
