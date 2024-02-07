74ffa28b2835aa21c573a86e3d786a8f43e494e5
Panayiotis Photiadis <pfotia01@ucy.ac.cy>
2024-02-07 02:58:11 +0200

    add data

---
+++ helping_code/cmd-opener.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define your directories
+dir1="C:\Users\pphot\Desktop\Thesis\docker"
+dir2="C:\Users\pphot\Desktop\Thesis\rasa-chatbot"
+dir3="C:\Users\pphot\Desktop\Thesis\rasa-chatbot"
+dir4="C:\Users\pphot\Desktop\Thesis\mongodb-api"
+dir5="C:\Users\pphot\Desktop\Thesis\scrapper"
+
+# Open cmd windows in specified directories
+cmd.exe /c start cmd.exe /k "cd $dir1"
+cmd.exe /c start cmd.exe /k "cd $dir2"
+cmd.exe /c start cmd.exe /k "cd $dir3"
+cmd.exe /c start cmd.exe /k "cd $dir4"
+cmd.exe /c start cmd.exe /k "cd $dir5"
